4d11236d992984126137013d5c63b411c91629a9
Jeff Montinard <68262228+jmontinard@users.noreply.github.com>
2022-09-27 20:16:13 -0400

    h
    h

--- src/day1/LinearSearchList.ts
+++ src/day1/LinearSearchList.ts
@@ -1,5 +1,5 @@
 export default function linear_search(haystack: number[], needle: number): boolean {
-for( i = 0; i < haystack.length; ++i ) { 
+for( let i = 0; i < haystack.length; ++i ) { 
     
     if( haystack[i] === needle ) {
         return true;
